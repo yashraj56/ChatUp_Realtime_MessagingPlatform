c26df30ebcb29b4de4c6ff089f48128172d4db7a
Yash_Raj <yashrajisha123@gmail.com>
2023-04-25 12:40:23 -0700

    Changing 25-04

--- lib/Screens/Authentication_/LoginScreen_.dart
+++ lib/Screens/Authentication_/LoginScreen_.dart
@@ -12,9 +12,7 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-
-
-
+// SubClass
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
