d64b40f334be18ac6cb15dc829ff4ba582268c06
Yash_Raj <yashrajisha123@gmail.com>
2023-04-26 11:33:29 -0700

    Changing 26-04

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:minorproject/Screens/Splash_.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-// yash gandu
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   _initializeFirebase();
